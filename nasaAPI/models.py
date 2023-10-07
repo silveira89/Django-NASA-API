05d7f681370a2a6ff0bf3590ca833e9c9d2d9b94
Andre Silveira <alasandre@gmail.com>
2023-10-07 11:13:41 -0300

    Modificações no model NasaJson.

--- nasaAPI/models.py
+++ nasaAPI/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 
 class NasaJson(models.Model):
-    date = models.DateField()
+    date = models.CharField(max_length=20)
     explanation = models.TextField(max_length=2000)
-    hdurl = models.CharField(200)
-    mediaType = models.CharField(20)
-    serviceVersion = models.CharField(20)
-    title = models.CharField(200)
-    url = models.CharField(200)
+    hdurl = models.CharField(max_length=200)
+    mediaType = models.CharField(max_length=20)
+    serviceVersion = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
