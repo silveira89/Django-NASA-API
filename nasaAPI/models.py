22af122ab5c526d968230f2aae5780067af10d18
Andre Silveira <alasandre@gmail.com>
2023-10-07 11:02:46 -0300

    Criação do model NasaJson.

--- nasaAPI/models.py
+++ nasaAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class NasaJson(models.Model):
+    date = models.DateField()
+    explanation = models.TextField(max_length=2000)
+    hdurl = models.CharField(200)
+    mediaType = models.CharField(20)
+    serviceVersion = models.CharField(20)
+    title = models.CharField(200)
+    url = models.CharField(200)
